f5efcc0a4d9d4696e27268d5709d5e0f8f0008ca
Alexander Tkachuk <tksasha@yandex.ru>
2010-07-29 10:57:37 +0400

    update rails configs

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = 'f5c5f047f6972edcf1e317620c0e2f40d8fc3ad90cf5a2b127d00b373a04caa0c388dbe6aa0789fd451218548bfec7ce6893edff169e94d0c303d2010ac3a106'
+Balans::Application.config.secret_token = '153b687654fcd675e2d8e92db5d74b47bde7da1a3d8a0116a531704ccf98cccf045513504250329d92536b120c63f6786dd3fe5978dce8567e51c77c2c24b94d'
