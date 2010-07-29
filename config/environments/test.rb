e3551eabf74caa8ffbbf41cde5641ed9f90b0597
Alexander Tkachuk <tksasha@yandex.ru>
2010-07-29 10:59:11 +0400

    update rails configs

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-Balans3::Application.configure do
+Balans::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
   # The test environment is used exclusively to run your application's
@@ -30,5 +30,6 @@ Balans3::Application.configure do
   # like if you have constraints or database-specific column types
   # config.active_record.schema_format = :sql
 
+  # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 end
