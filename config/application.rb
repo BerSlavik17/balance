d8bd3994d2f22aa04fff808d1347ddc276cda70b
Alexander Tkachuk <tksasha@yandex.ru>
2010-07-29 11:03:08 +0400

    update rails

--- config/application.rb
+++ config/application.rb
@@ -6,20 +6,20 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
 
-module Balans3
+module Balans 
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    # Add additional load paths for your own custom dirs
-    # config.load_paths += %W( #{config.root}/extras )
+    # Custom directories with classes and modules you want to be autoloadable.
+    # config.autoload_paths += %W(#{config.root}/extras)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named
+    # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-    # Activate observers that should always be running
+    # Activate observers that should always be running.
     # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
@@ -32,10 +32,13 @@ module Balans3
 
     # Configure generators values. Many other options are available, be sure to check the documentation.
     config.generators do |g|
-    #   g.orm             :active_record
-    #   g.template_engine :erb
+      g.orm             :active_record
+      g.template_engine :erb
       g.test_framework  :test_unit, :fixture => false, :fixture_replacement => :factory_girl
     end
+
+    # JavaScript files you want as :defaults (application.js is always included).
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
@@ -44,3 +47,4 @@ module Balans3
     config.filter_parameters += [:password]
   end
 end
+
