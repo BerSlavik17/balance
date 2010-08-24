99524255d2e43c57db7b3a703196ebfa92e599e3
Alexander Tkachuk <tksasha@yandex.ru>
2010-08-24 17:01:58 +0400

    переписал логику подсчета баланса с учетом выноса at_begin в Setting

--- app/models/cash.rb
+++ app/models/cash.rb
@@ -1,15 +1,11 @@
 class Cash < ActiveRecord::Base
   def self.at_begin
-    195193.96
+    Setting.at_begin.value.to_f #195193.96
   end
 
   def self.at_end
     # округлим до двух знаков после точки
     ('%.2f' % (self.at_begin + Item.income.sum(:sum) - Item.expense.sum(:sum))).to_f
-  end
-
-  def sum
-    '%.2f' % self[:sum]
   end
 
   def self.balans
--- test/unit/cash_test.rb
+++ test/unit/cash_test.rb
@@ -1,8 +1,32 @@
 require 'test_helper'
 
 class CashTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test 'at_begin' do
+    assert_equal Setting.at_begin.value.to_f, Cash.at_begin
+  end
+
+  test 'at_end' do
+    fill_db
+
+    assert_equal (100 + 9 - 5 - 6), Cash.at_end
+  end
+
+  test 'balans' do
+    fill_db
+
+    assert_equal (Cash.sum(:sum) - Cash.at_end), Cash.balans
+  end
+
+  private
+  def fill_db
+    food   = Factory :category, :name => 'food',   :income => false 
+    sex    = Factory :category, :name => 'sex',    :income => false
+    salary = Factory :category, :name => 'salary', :income => true 
+
+    Setting.set 'at_begin', '100'
+    
+    Factory :item, :summa => '5.00', :category => food
+    Factory :item, :summa => '6.00', :category => sex
+    Factory :item, :summa => '9.00', :category => salary
   end
 end
