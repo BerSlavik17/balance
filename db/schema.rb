1672bec7f084625c997268f7e306703f8bed88d5
Alexander Tkachuk <tksasha@yandex.ru>
2010-08-24 17:03:19 +0400

    добавил таблицу settings

--- db/schema.rb
+++ db/schema.rb
@@ -1,15 +1,16 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of Active Record to incrementally modify your database, and
-# then regenerate this schema definition.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your database schema. If you need
-# to create the application database on another system, you should be using db:schema:load, not running
-# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100613153607) do
+ActiveRecord::Schema.define(:version => 20100730060251) do
 
   create_table "cashes", :force => true do |t|
     t.float  "sum"
@@ -34,5 +35,12 @@ ActiveRecord::Schema.define(:version => 20100613153607) do
 
   add_index "items", ["date", "category_id"], :name => "index_balans_items_on_date_and_category_id"
   add_index "items", ["date"], :name => "index_balans_items_on_date"
+
+  create_table "settings", :force => true do |t|
+    t.string   "key"
+    t.string   "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
