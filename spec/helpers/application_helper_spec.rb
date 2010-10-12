249be4d6840d7c4a3358421cab60291f9359871e
Alexander Tkachuk <tksasha@yandex.ru>
2010-10-12 15:59:17 +0400

    если баланс больше -0.01 и меньше 0 то не выводить его с минусом

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,9 +2,23 @@ require 'spec_helper'
 
 describe ApplicationHelper do
   describe 'sum' do
-    subject { sum(40_000.690000) }
+    describe 'greater than zero' do
+      subject { sum(40_000.690000) }
 
-    it { should == '40&nbsp;000.69' }
+      it { should == '40&nbsp;000.69' }
+    end
+
+    describe 'greater than -0.01 and less than 0' do
+      subject { sum(-0.001) }
+
+      it { should == '0.00' }
+    end
+
+    describe 'less or equal -0.01 and less than 0' do
+      subject { sum(-0.01) }
+
+      it { should == '-0.01' }
+    end
   end
 end
 
