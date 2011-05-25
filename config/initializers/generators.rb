53e821ebf7dda583f238f1a93c5a1b976be8784b
Alexander Tkachuk <tksasha@yandex.ru>
2011-05-25 16:10:03 +0400

    переименовал initializer

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,5 +1,6 @@
 Balance::Application.configure do 
   config.generators do |g|
     g.test_framework :rspec, :fixtures => false, :fixture_replacement => :factory_girl
+    g.helper false
   end
 end
