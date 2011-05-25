cfb12a4b48de4589a46604e4ae788af98f3ad411
Alexander Tkachuk <tksasha@yandex.ru>
2011-05-25 16:07:22 +0400

    yamler теперь "syck"

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
+YAML::ENGINE.yamler = 'syck'
 
 module Balance 
   class Application < Rails::Application
