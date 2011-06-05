5d9510b72fd20eb859b7de49bb3d0d9adb386f08
Alexander Tkachuk <tksasha@ya.ru>
2011-06-05 12:55:49 +0400

    починил ошибку, когда dialog не закрывался после обновления записи

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,41 @@
 class ItemsController < InheritedResources::Base
+  include InheritedResources::DSL
+
   respond_to :js
   respond_to :html, :only => :index
 
   actions :all, :except => :index
 
-  def index
-    @cashes = Cash.scoped
+  before_filter :find_cashes, :find_items, :find_consolidates, :only => :index
 
-    respond_to do |format|
-      format.js do
-        @items = Item.search_by params
-        @consolidates = Item.consolidates params
-      end
-
-      format.html
+  create! do |success, failure|
+    success.js do
+      # в before_filter это выносить не стоит. 
+      # потому что пересчет сводного отчета должен происходить 
+      # после успешного создания записи
+      @consolidates = find_consolidates
     end
+  end
+
+  update! do |success, failure|
+    success.js do
+      # в before_filter это выносить не стоит. 
+      # потому что пересчет сводного отчета должен происходить 
+      # после успешного обновления записи
+      @consolidates = find_consolidates
+    end
+  end
+
+  private
+  def find_cashes
+    @cashes = Cash.scoped
+  end
+
+  def find_items
+    @items = Item.search_by params
+  end
+
+  def find_consolidates
+    @consolidates = Item.consolidates params
   end
 end
