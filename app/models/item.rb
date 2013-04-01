95aaf58811687428c9d897e41cf28383636506cf
Alexander Tkachuk <tksasha@ya.ru>
2013-04-01 10:36:49 +0400

    Item.income; Item.expense

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,15 @@
-require 'date_range'
-
 class Item < ActiveRecord::Base
   belongs_to :category
 
-  before_save :calculate_sum
+  validates :date, :category_id, :presence => true
+
+  scope :income, -> { includes(:category).where('categories.income' => true) }
+
+  scope :expense, -> { includes(:category).where('categories.income' => false) }
+
+  acts_as_paranoid
+
+  before_validation :calculate_sum
 
   private
   def calculate_sum
@@ -12,69 +18,12 @@ class Item < ActiveRecord::Base
 
   class << self
     def search
-      where(date: Date.today.beginning_of_month..Date.today.end_of_month).order('created_at DESC')
+      from = Date.today.beginning_of_month
+
+      to = Date.today.end_of_month
+
+      where(date: from..to).select('SUM(sum) AS sum, date, category_id').group('date, category_id').
+        order('date DESC')
     end
   end
 end
-
-__END__
-  #acts_as_paranoid
-
-  delegate :year, :month, :day, :to => :date
-
-  delegate :name, to: :category, prefix: true, allow_nil: true
-
-  scope :income, 
-    includes(:category).where('categories.income IN(?)', [1, true])
-
-  scope :expense,
-    includes(:category).where('categories.income IN(?)', [0, false])
-
-  #TODO: spec it
-  default_scope where(:deleted_at => nil)
-
-  belongs_to :category
-
-  validates :date, :sum, :category_id, :presence => true
-
-  validates :summa, :format => { :with => /^[0-9\+\-\.]+$/ }
-
-  before_validation :calculate_sum
-
-  def self.consolidates params={}
-    items = where(:date => DateRange::month(params))
-    items = items.select('SUM(sum) AS sum, category_id, categories.name AS c_name, categories.income AS income, categories.url AS url')
-    items = items.group(:category_id)
-    items = items.joins(:category)
-    items = items.order('categories.income DESC')
-    
-    items.map do |item|
-      { :category_name => item.c_name, :sum => item.sum, :income => item.income?, :category_url => item.url }
-    end
-  end
-
-  def self.search_by params={}
-    items = where :date => DateRange::month(params)
-
-    if params[:category]
-      items = items.where(:category_id => Category.find_by_url(params[:category]))
-    end
-
-    items = items.order('date DESC')
-    items = items.includes('category')
-  end
-
-  private
-  def calculate_sum
-    #FIXME: "не ломаться" когда тип значение summa отличное от String
-    self.sum = eval(self.summa.gsub(/[^0-9\+\-\.]/, ''))
-  end
-
-  class << self
-    # TODO: spec me
-    def search *args
-      order('created_at DESC').limit(100)
-    end
-  end
-end
-
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,87 +1,75 @@
 require 'spec_helper'
 
 describe Item do
+  it { should validate_presence_of :date }
+
+  it { should validate_presence_of :category_id }
+
   it { should belong_to :category }
+
+  its(:class) { should be_paranoid }
 
   describe '.search' do
     before do
-      Item.should_receive(:where).with(date: Date.today.beginning_of_month..Date.today.end_of_month) do
-        double.tap { |a| a.should_receive(:order).with('created_at DESC') }
+      from = Date.today.beginning_of_month
+
+      to = Date.today.end_of_month
+
+      #
+      # Item.where(from..to).
+      #   select('SUM(sum) AS sum, date, category_id').
+      #   group('date, category_id').
+      #   order('date DESC')
+      #
+      Item.should_receive(:where).with(date: from..to) do
+        double.tap do |a|
+          a.should_receive(:select).with('SUM(sum) AS sum, date, category_id') do
+            double.tap do |b|
+              b.should_receive(:group).with('date, category_id') do
+                double.tap { |c| c.should_receive(:order).with('date DESC') }
+              end
+            end
+          end
+        end
       end
     end
 
     it { expect { Item.search }.to_not raise_error }
   end
 
-  describe 'calculate sum' do
+  describe '.income' do
     before do
-      subject.summa = '2 + 2'
-      
-      subject.send :calculate_sum
+      Item.should_receive(:includes).with(:category) do
+        double.tap { |a| a.should_receive(:where).with('categories.income' => true) }
+      end
     end
 
-    its(:sum) { should eq 4 }
+    it { expect { Item.income }.to_not raise_error }
   end
 
-  context 'before save callbacks' do
+  describe '.expense' do
+    before do
+      Item.should_receive(:includes).with(:category) do
+        double.tap { |a| a.should_receive(:where).with('categories.income' => false) }
+      end
+    end
+
+    it { expect { Item.expense }.to_not raise_error }
+  end
+
+  describe 'calculate_sum' do
+    before do
+      subject.summa = '1.8+2.4'
+
+      subject.should_receive(:sum=).with(4.2)
+    end
+
+    it { expect { subject.send :calculate_sum }.to_not raise_error }
+  end
+
+  context 'before_validation callbacks' do
     before { subject.should_receive :calculate_sum }
 
-    it { expect { subject.save }.to_not raise_error }
-  end
-
-  pending 'fixme' do
-  describe 'consolidates' do
-    before {
-      @food   = Factory :category, :name => 'food'
-      @sex    = Factory :category, :name => 'sex'
-      @salary = Factory :category, :name => 'salary', :income => true
-
-      Factory :item, :date => Date.new(2010,9,1), :summa => '43.00', :category => @sex
-      Factory :item, :date => Date.today.beginning_of_month, :summa => '15.00', :category => @food
-      Factory :item, :date => Date.today.end_of_month, :summa => '20.00', :category => @food
-      Factory :item, :date => Date.today, :summa => '400', :category => @salary
-      Factory :item, :date => Date.today, :summa => '16.90', :category => @sex
-    }
-
-    describe 'for default date' do
-      subject { Item.consolidates }
-
-      def consolidates
-        [
-          { :category_name => @salary.name, :sum => 400, :income => @salary.income?, :category_url => 'salary' }, 
-          { :category_name => @food.name, :sum => 15.0 + 20.0, :income => @food.income?, :category_url => 'food' }, 
-          { :category_name => @sex.name, :sum => 16.9, :income => @sex.income?, :category_url => 'sex' } 
-        ]
-      end
-
-      it { should == consolidates }
-    end
-
-    describe 'for custom date' do
-      subject { Item.consolidates(:month => 9, :year => 2010) }
-
-      def consolidates
-        [{ :category_name => @sex.name, :sum => 43.0, :income => @sex.income?, :category_url => 'sex' }]
-      end
-      
-      it { should == consolidates }
-    end
-  end
-
-  describe 'destroy' do
-    before {
-      @one    = Factory :item
-      @two    = Factory :item
-      @three  = Factory :item
-
-      @two.destroy
-    }
-    
-    subject { Item.scoped }
-
-    it 'should not select' do
-      should == [@one, @three]
-    end
-  end
+    it { expect { subject.valid? }.to_not raise_error }
   end
 end
