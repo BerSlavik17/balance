acfa5f093f9755550c14658162bac55183ca3075
Alexander Tkachuk <tksasha@ya.ru>
2013-04-12 10:42:53 +0400

    add inherited resources to ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,31 +1,9 @@
-class ItemsController < ApplicationController
-  helper_method :collection, :resource, :build_resource
-
-  before_filter :build_resource, only: :create
-
-  def create
-    if resource.save
-      render :create
-    else
-      render :new
-    end
-  end
-
-  def update
-    if resource.update_attributes params[:item]
-      redirect_to :root
-    else
-      render :edit
-    end
-  end
+class ItemsController < InheritedResources::Base
+  respond_to :html, :js
 
   private
   def collection
     @items ||= ItemsDecorator.new Item.search
-  end
-
-  def build_resource
-    @item ||= Item.new params[:item]
   end
 
   def resource
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe ItemsController do
-  before { Item.stub find: item }
+  it { should be_a InheritedResources::Base }
 
-  let(:item) { stub_model Item }
+  its(:mimes_for_respond_to) { should include :html }
+  
+  its(:mimes_for_respond_to) { should include :js }
 
   it { should route(:post, '/items').to(action: :create) }
 
@@ -25,62 +27,4 @@ describe ItemsController do
 
   it { should route(:get, '/2008/05/17/food').
     to(action: :index, year: '2008', month: '05', day: '17', category: 'food') }
-
-  it { should have_helper_method :collection }
-
-  it { should have_helper_method :build_resource }
-
-  it { should have_helper_method :resource }
-
-  its(:build_resource) { should be_a Item }
-    
-  describe 'GET index' do
-    before { get :index }
-
-    it { should render_template :index }
-  end
-
-  describe 'POST create as JS' do
-    before { Item.any_instance.stub save: true }
-
-    before { post :create, format: :js }
-
-    it { should render_template :create }
-
-    it { should respond_with_content_type :js }
-  end
-
-  describe 'POST create as JS with invalid attributes' do
-    before { Item.any_instance.stub save: false }
-
-    before { post :create, format: :js }
-
-    it { should render_template :new }
-
-    it { should respond_with_content_type :js }
-  end
-
-  describe 'GET edit' do
-    before { get :edit, id: item }
-
-    it { should render_template :edit }
-
-    its(:resource) { should eq item }
-  end
-
-  describe 'PUT update' do
-    before { item.stub update_attributes: true }
-
-    before { put :update, id: item }
-
-    it { should redirect_to :root }
-  end
-
-  describe 'PUT update with invalid attributes' do
-    before { item.stub update_attributes: false }
-
-    before { put :update, id: item }
-
-    it { should render_template :edit }
-  end
 end
