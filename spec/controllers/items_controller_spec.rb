d4cae0229c60fa0abe421b2ad27a8df31bafb67a
Alexander Tkachuk <tksasha@ya.ru>
2013-04-12 10:36:15 +0400

    remove pendings

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -11,6 +11,21 @@ describe ItemsController do
 
   it { should route(:put, '/items/1').to(action: :update, id: 1) }
 
+  it { should route(:get, '/2011').to(action: :index, year: '2011') }
+  
+  it { should route(:get, '/2011').to(action: :index, year: '2011') }
+
+  it { should route(:get, '/2013/02').to(action: :index, year: '2013', month: '02') }
+
+  it { should route(:get, '/2012/03/17').to(action: :index, year: '2012', month: '03', day: '17') }
+
+  it { should route(:get, '/2010/food').to(action: :index, year: '2010', category: 'food') }
+
+  it { should route(:get, '/2009/04/salary').to(action: :index, year: '2009', month: '04', category: 'salary') }
+
+  it { should route(:get, '/2008/05/17/food').
+    to(action: :index, year: '2008', month: '05', day: '17', category: 'food') }
+
   it { should have_helper_method :collection }
 
   it { should have_helper_method :build_resource }
