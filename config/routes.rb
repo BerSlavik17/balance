2bdeb64ac34866095f4907f49fac48f795ad1290
Alexander Tkachuk <tksasha@ya.ru>
2013-06-26 10:46:11 +0400

    allow destroy item

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Balance::Application.routes.draw do
-  resources :items, only: [:create, :edit, :update]
+  resources :items, only: [:create, :edit, :update, :destroy]
 
   constraints :year => /\d{4}/, :month => /\d{1,2}/, :day => /\d{1,2}/, :category => /[a-z_]+/ do
     get '/(:year(/:month)(/:category))' => 'items#index'
