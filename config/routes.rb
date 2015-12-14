a2d022d481e68d41104fd2e6e15c8e7b08843b10
Alexander Tkachuk <mail@tksasha.me>
2015-12-14 14:02:57 +0200

    hot bugfix; add `TODO`

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Balance::Application.routes.draw do
   resources :items, only: [:create, :edit, :update, :destroy]
 
-  constraints :year => /\d{4}/, :month => /\d{1,2}/, :day => /\d{1,2}/, :category => /[a-z_]+/ do
+  #
+  # TODO: delme
+  #
+  constraints :year => /\d{4}/, :month => /\d{1,2}/, :day => /\d{1,2}/, :category => /[a-z_-]+/ do
     get '/(:year(/:month)(/:category))' => 'items#index'
   end
 
